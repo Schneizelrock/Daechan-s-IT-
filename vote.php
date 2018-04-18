057d16b7497e0dc5daed91a78a205110b180bbc2
Schneizelrock <rakeshshaw114@gmail.com>
2018-04-18 13:39:46 +0530

    Create vote.php

---
+++ vote.php
@@ -0,0 +1,17 @@
+<html>
+<head><title>Show Candidates</title>
+<script>
+
+</script>
+</head>
+<body>
+<form id="myform">
+<input type="radio" id="na" name="Nominee_1" value="You have voted ABC">ABC<br>
+<input type="radio" id="na" name="Nominee_2" value="You have voted XYZ">XYZ<br>
+<input type="radio" id="na" name="Nominee_3" value="You have voted POR">POR<br>
+<input type="button" value="Record Vote" onclick="getValidate();"/>
+<div id="check" class="displaybox">
+</div>
+</form>
+</body>
+</html>
